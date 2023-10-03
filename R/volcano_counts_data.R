c43ac4edefa583eba1afea98d1addd5026edb44b
Akanksha Shridharani <ashr0017@student.monash.edu>
2023-10-03 12:10:40 +1100

    Added the second(volcano) raw data file for the package

---
+++ R/volcano_counts_data.R
@@ -0,0 +1,35 @@
+#' @title Dataset about different types of volcanoes
+#'
+#' A dataset that contains the the different types of volcanoes worldwide, their elevation to help us knowing whether its a nature's fury or geological wonder
+#'
+#' @format A data frame with 958 rows and 26 variables.
+#' \describe{
+#'   \item{volcano_number}{Volcano unique ID}
+#'   \item{volcano_name}{Volcano Name}
+#'   \item{primary_volcano_type	}{Types of Volcano}
+#'   \item{last_eruption_year}{Last Year Erupted}
+#'   \item{country}{Country}
+#'   \item{region}{Region}
+#'   \item{subregion}{Sub Region}
+#'   \item{latitude}{Latitude}
+#'   \item{longitude}{Longitude}
+#'   \item{elevation}{Elevation}
+#'   \item{tectonic_settings}{Volcano Plate Tectonic Settings}
+#'   \item{evidence_category}{Type of Evidence}
+#'   \item{major_rock_1}{Major Rock Type}
+#'   \item{major_rock_2}{Major Rock Type}
+#'   \item{major_rock_3}{Major Rock Type}
+#'   \item{major_rock_4}{Major Rock Type}
+#'   \item{major_rock_5}{Major Rock Type}
+#'   \item{minor_rock_1}{Minor Rock Type}
+#'   \item{minor_rock_2}{Minor Rock Type}
+#'   \item{minor_rock_3}{Minor Rock Type}
+#'   \item{minor_rock_4}{Minor Rock Type}
+#'   \item{minor_rock_5}{Minor Rock Type}
+#'   \item{population_within_5_km}{Total population within 5 km of volcano}
+#'   \item{population_within_10_km}{Total population within 10 km of volcano}
+#'   \item{population_within_30_km}{Total population within 30 km of volcano}
+#'   \item{population_within_100_km}{Total population within 100 km of volcano}
+#' }
+#' @source \url{https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/volcano.csv}
+"eruption_counts"
--- data-raw/volcano_counts.R
+++ data-raw/volcano_counts.R
@@ -1,3 +1,6 @@
-## code to prepare `volcano_counts` dataset goes here
+library(janitor)
+library(tidyverse)
+
+volcano_counts <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/volcano.csv')
 
 usethis::use_data(volcano_counts, overwrite = TRUE)
